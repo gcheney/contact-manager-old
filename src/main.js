fb0ad3c9971bcddfb1350829d6aeab96163c5876
Glendon Cheney <glendon.cheney@gmail.com>
2017-02-02 21:08:56 -0600

    Add common header file

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/home/homePage');
 var About = require('./components/about/aboutPage');
+var Header = require('./common/header');
 
 (function(win) {
     'use strict';
@@ -20,6 +21,7 @@ var About = require('./components/about/aboutPage');
 
             return (
                 <div>
+                    <Header />
                     <Child />
                 </div>
             );
