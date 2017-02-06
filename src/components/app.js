46cadb3501b670513185689465db820fcdbf8845
Glendon Cheney <glendon.cheney@gmail.com>
2017-02-06 12:59:55 -0600

    Reconfigure routes with React Router

---
+++ src/components/app.js
@@ -0,0 +1,21 @@
+/*eslint-disable strict */ //global var needed for jQuery/Bootstrap
+
+var React = require('react');
+var RouteHandler = require('react-router').RouteHandler;
+var Header = require('../common/header');
+$ = jQuery = require('jquery');
+
+var App = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <Header />
+                <div className="container-fluid">
+                    <RouteHandler />
+                </div>
+            </div>
+        );
+    }
+});
+
+module.exports = App;
--- src/main.js
+++ src/main.js
@@ -1,42 +1,8 @@
-$ = jQuery = require('jquery');
 var React = require('react');
-var Home = require('./components/home/homePage');
-var AboutPage = require('./components/about/aboutPage');
-var ContactPage = require('./components/contacts/contactPage');
-var Header = require('./common/header');
+var Router = require('react-router');
+var routes = require('./routes');
 
-(function(win) {
-    'use strict';
+Router.run(routes, function(Handler) {
+    React.render(<Handler/>, document.getElementById('app'));
+});
 
-    var App = React.createClass({
-        render: function() {
-            var Child; 
-
-            switch (this.props.route) {
-                case 'about':
-                    Child = AboutPage;
-                    break;
-                case 'contacts':
-                    Child = ContactPage;
-                    break;
-                default:
-                    Child = Home;
-            }
-
-            return (
-                <div>
-                    <Header />
-                    <Child />
-                </div>
-            );
-        }
-    });
-
-    function render() {
-        var route = win.location.hash.substr(1);
-        React.render(<App route={route} />, document.getElementById('app'));
-    }
-
-    win.addEventListener('hashchange', render);
-    render();
-})(window);
